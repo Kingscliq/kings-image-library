bae584d8720aee7b1a46d25737dc666ad580b855
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2020-09-13 08:05:18 +0100

    initial commit

---
+++ precache-manifest.78128a5c506284d6fa1b497daeb2fa4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a26c2c48b4401cbc34ca6ad7b3f3ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1691f28b978ea8ac0d2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dc95c4de7d632a075757",
+    "url": "/static/js/2.7ec9afcc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7ec9afcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1691f28b978ea8ac0d2",
+    "url": "/static/js/main.8287193b.chunk.js"
+  },
+  {
+    "revision": "f665cb94705c0417fde4",
+    "url": "/static/js/runtime-main.1027ea0a.js"
+  }
+]);
